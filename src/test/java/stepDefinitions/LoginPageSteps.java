519b0d2c2388b360e3859e4675621f1d7abed063
Durvesh <durveshpokade835@gmail.com>
2024-09-19 23:15:33 +0530

    BDD_Framework Implementation

---
+++ src/test/java/stepDefinitions/LoginPageSteps.java
@@ -0,0 +1,95 @@
+package stepDefinitions;
+
+import org.junit.Assert;
+
+import com.pages.LoginPage;
+import com.qa.Factory.DriverFactory;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageSteps {
+
+	private static String title;
+	private LoginPage loginPage = new LoginPage(DriverFactory.getDriver());
+
+	@Given("the user is on the login page")
+	public void the_user_is_on_the_login_page() {
+		// Write code here that turns the phrase above into concrete actions
+		DriverFactory.getDriver()
+		.get("https://pharmacist-dev.arine.io/");
+		
+//		loginPage.openLoginPage();
+		title = loginPage.getCurrentUrl();
+		System.out.println(title);
+//		Assert.assertTrue(title.equalsIgnoreCase("https://pharmacist-dev.arine.io/"));
+
+	}
+
+	@When("the user enters the valid email {string}")
+	public void the_user_enters_the_valid_email(String userName) {
+		// Write code here that turns the phrase above into concrete actions
+
+		loginPage.enterEmail(userName);
+
+	}
+
+	@When("valid password {string}")
+	public void valid_password(String userPassword) {
+		// Write code here that turns the phrase above into concrete actions
+		loginPage.enterPassword(userPassword);
+
+	}
+
+	@Then("the user should be logged in")
+	public void the_user_should_be_logged_in() {
+		// Write code here that turns the phrase above into concrete actions
+		boolean isDashBoardVisible = loginPage.isDashboardDisplayed();
+		Assert.assertTrue(isDashBoardVisible);
+		
+	}
+
+	@When("the user enters invalid password {string}")
+	public void the_user_enters_invalid_password(String invalidPassword) {
+		// Write code here that turns the phrase above into concrete actions
+		loginPage.enterPassword(invalidPassword);
+
+//		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("an error message should be displayed {string}")
+	public void an_error_message_should_be_displayed(String expErrorMsg) {
+		// Write code here that turns the phrase above into concrete actions
+		String actualErrorMsg = loginPage.getErrorMessage();
+		Assert.assertTrue(actualErrorMsg.contains(expErrorMsg));
+//		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user enters an invalid email {string}")
+	public void the_user_enters_an_invalid_email(String invalidUserName) {
+		// Write code here that turns the phrase above into concrete actions
+		loginPage.enterEmail(invalidUserName);
+
+//		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks on Login button")
+	public void user_clicks_on_login_button() {
+		// Write code here that turns the phrase above into concrete actions
+		loginPage.clickSignIn();
+
+		
+	}
+
+	@Then("I should see an error message {string}")
+	public void i_should_see_an_error_message(String expErrorMsg2) {
+		// Write code here that turns the phrase above into concrete actions
+
+		String actualErrorMsg = loginPage.getErrorMessage();
+		Assert.assertTrue(actualErrorMsg.contains(expErrorMsg2));
+
+//		throw new io.cucumber.java.PendingException();
+	}
+
+}
---
+++ src/test/resources/AppFeatures/LoginPage.feature
@@ -0,0 +1,26 @@
+Feature: Pharmacist Login
+
+  Scenario: Successful login with valid credentials
+    Given the user is on the login page
+    When the user enters the valid email "hardstop-pharmacist6@mailinator.com"
+    And valid password "Password1#"
+    And user clicks on Login button
+    Then the user should be logged in
+   
+    
+
+
+    Scenario: Login with invalid password and verify error message
+
+    Given the user is on the login page
+    When the user enters the valid email "hardstop-pharmacist6@mailinator.com"
+    And the user enters invalid password "InvalidPassword"
+    And user clicks on Login button
+    Then an error message should be displayed "Wrong username/password"
+    
+    Scenario: Login with invalid email and verify error message
+    Given the user is on the login page
+    When the user enters an invalid email "hardstop-pharmacist6@mailinator.co"
+    And valid password "Password1#"
+    And user clicks on Login button
+    Then I should see an error message "User not found"
